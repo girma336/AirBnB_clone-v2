fa1cde857eb5c774bb37bbd9cef8e5a067dc44ef
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-09-14 13:03:46 +0300

    all

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
+#!/usr/bin/python
+""" holds class City"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -14,6 +14,11 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
     else:
         state_id = ""
         name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)

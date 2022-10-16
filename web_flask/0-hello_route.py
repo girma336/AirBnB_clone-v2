df5bc5585005ceab709cd2803e334fee447dc05a
girma <girmatarekegn.gi@gmail.com>
2022-10-16 05:31:42 +0300

    Add hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ start a Flask web app
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """returns hello HBNB!"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')

d95a5e419dfd59847afa920384ed6e06d991391a
girma <girmatarekegn.gi@gmail.com>
2022-10-16 06:09:07 +0300

    Add python is magic

---
+++ web_flask/3-python-route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+start a Flask web app
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return HBNB!"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Return c and given text"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text):
+    """return python and additional text"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
